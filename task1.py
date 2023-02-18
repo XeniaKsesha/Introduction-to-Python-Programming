1efde6970c0bae0ae2a5beade48468154585a9aa
Ксеша <bouncywolf@list.ru>
2023-02-18 16:38:39 +0300

    done task 1

--- task1.py
+++ task1.py
@@ -7,3 +7,6 @@ if number > 99 and number < 1000:
     num3 = supportnum // 10
     sum = num1 + num2 + num3
     print(f'Сумма отдельных чисел в числе {number} будет равняться {sum}')
+
+else:
+    print('Вы ввели неверное число')
