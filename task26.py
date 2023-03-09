ddf02157fde547da680532a3a71479aae957185a
Ксеша <bouncywolf@list.ru>
2023-03-09 14:42:13 +0300

    done task 26

--- task26.py
+++ task26.py
@@ -1,8 +1,8 @@
 def raise_to_the_power(number1, number2):
-    power = number1**number2
-    return power
+    if number2 == 0:
+          return 1
+    return number1 * raise_to_the_power(number1, number2 - 1)
 
 number1 = int(input('Введите число, которое будет возводиться в степень: '))
 number2 = int(input('Введите степень числа: '))
-raise_to_the_power(number1, number2)
-print(f'При возведении числа {number1} в степень {number2} будет {raise_to_the_power(number1, number2)}')
+print(raise_to_the_power(number1, number2))
