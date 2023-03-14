fc0a935fee85983917d0b03b6cf211cea17ba035
Ксеша <bouncywolf@list.ru>
2023-03-14 12:44:26 +0300

    done task 36

--- task36.py
+++ task36.py
@@ -2,9 +2,9 @@
 
 def print_operation_table(operation, num_rows=6, num_columns=6):
     for i in range(1, num_rows + 1):
-        answer = []
+        arr = []
         for j in range(1, num_columns + 1):
-            answer.append(str(operation(i, j)))
-        print(''.join(f'{e:<4}' for e in answer))
+            arr.append(str(operation(i, j)))
+        print(''.join(f'{_:<4}' for _ in arr))
 
 print_operation_table(lambda x, y: x * y)
